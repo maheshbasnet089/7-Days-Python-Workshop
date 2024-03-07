2fb9d554059ccd15652bd2ef5439720bed06ea75
maheshbasnet089 <basnetmanish089@gmail.com>
2024-03-07 18:41:34 +0545

    first commit

---
+++ day1.py
@@ -0,0 +1,23 @@
+cup = "coffee"
+gamala = "ful"
+age = "21c"
+is_nepali = False
+first_name = "Manish"
+
+
+print("my first_name is", first_name)
+print("my first_name is "+ first_name)
+print(f"my first_name is {first_name}")
+print("my first_name is {}".format(first_name))
+
+
+friend1 = "Bob Marley"
+friend2 = "Pranjal Sharma"
+friend3 = "Anup Sawa limbu"
+
+#list
+friends = ["Bob Marley","Pranjal Sharma",1,True]
+print(friends[-1])
+
+
+
