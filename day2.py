b1e056eb4948f515fa05e065bb54e193974c59f1
maheshbasnet089 <basnetmanish089@gmail.com>
2024-03-10 17:57:30 +0545

    add day2

---
+++ day2.py
@@ -0,0 +1,48 @@
+
+
+
+# test = {
+#     'key' : 'value',
+#     'key2' : 'value2'
+# }
+
+# friends = ['Pranjal','Bob','Anup','Bikram','Priyanshu','Aayush']
+
+# # friends.insert(1,'Anup')
+# # friends.append('Anup')
+# # friends.append("Bhojraj")
+# # friends.pop(0)
+
+
+
+# # print(friends[0:4])
+# print(len(friends))
+
+
+
+friend = {
+    'name' : 'Pranjal',
+    'age' : 22
+} 
+
+# if 'haha' in friend:
+#     print("name exists")
+
+
+# del friend['age']
+# print(friend)
+# friend['isNepali'] = True
+
+# print(friend['name'])
+# print(friend['age'])
+
+
+# print(friend.keys())
+# print(friend.values())
+
+# my_tuple = (1,2,1,2,"Apple",3.14)
+
+# my_tuple[0] = 'hahaha'
+
+my_set = {1,2,3,4,1,2}
+print(my_set)
