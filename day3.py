b96477b8cb15f9609e21698793a556f22c1b2d48
maheshbasnet089 <basnetmanish089@gmail.com>
2024-03-11 17:58:19 +0545

    learn about conditions,loops

---
+++ day3.py
@@ -0,0 +1,55 @@
+# x = -10
+
+# if x > 0:
+#     print("x is positive")
+# elif x==0:
+#     print("x is zero")
+# else:
+#     print("x is negative")
+
+# x = 10 
+# y = -2
+
+# if x>0 or y>0:
+#     print("Both are positive")
+
+
+# fruits = ['apple','banana','mango']
+
+# for hello in fruits:
+#     print(hello)
+
+# colors = ('red','green','yellow')
+# for color in colors:
+#     print(color)
+
+# unique_numbers = {1,2,3,4,5}
+# for number in unique_numbers:
+#     print(number)
+
+student_grades = {
+    'Pranjal' : 90,
+    'Bob' : 50,
+    'Manish' : 30
+}
+# print(student_grades.values())
+# print(student_grades.items())
+# print(student_grades['Pranjal'])
+# print(student_grades['Bob'])
+# "Pranjal scored 90"
+# for student in student_grades:
+#     print(student,"scored",student_grades[student])
+
+# for student,grade in student_grades.items():
+#     print(student,"scored",grade)
+
+# for value in student_grades.values():
+#     print(value)
+
+# message = "This much for today!"
+
+# for char in message:
+#     print(char)
+
+for num in range(1,51):
+    print(num)
